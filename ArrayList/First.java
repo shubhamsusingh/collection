7d3f42e27c283998a63afe4938a6976fc8e7f8dd
shubham Singh <singhshubham68738@gmail.com>
2025-03-04 19:07:01 +0530

    ArrayList

---
+++ ArrayList/First.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class First{
+    public static void main(String[] args) {
+        ArrayList<Integer> al= new ArrayList<>();
+        al.add(1);
+        al.add(2);
+        al.add(3);
+        al.add(4);
+        al.add(5);
+        System.out.println(al);
+        al.addLast(12);
+        al.addFirst(0);
+        System.out.println(al);
+        al.set(3, 11);
+        System.out.println(al);
+        al.remove(3);
+        System.out.println(al);
+    }
+}
