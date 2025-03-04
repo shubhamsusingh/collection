d22da72219e7b6ce3efc0cb692d0c2f637ea5b3f
shubham singh <singhshubham68738@gmail.com>
2025-03-05 01:14:36 +0530

    working

--- ArrayList/First.java
+++ ArrayList/First.java
@@ -1,21 +1,48 @@
 import java.util.*;
-public class First{
+
+public class First {
     public static void main(String[] args) {
-        ArrayList<Integer> al= new ArrayList<>();
+        ArrayList<Integer> al = new ArrayList<>();
+        ArrayList<Integer> al1 = new ArrayList<>();
+        // Add:-
         al.add(1);
         al.add(2);
         al.add(3);
         al.add(4);
         al.add(5);
-        System.out.println(al);
-        al.addLast(12);
         al.addFirst(0);
+        al.addLast(6);
         System.out.println(al);
-        al.set(3, 11);
+        al1.add(1);
+        al1.add(6);
+        al1.add(7);
+        System.out.println(al1);
+        al.addAll(al1);
         System.out.println(al);
-        al.remove(3);
+        System.out.println("Add all Colletion on Specific Index");
+        al.addAll(3, al1);
+        System.out.println(al);
+
+        // Get:-
+        System.out.println("get Data");
+        int idx2 = al.get(2);
+        System.out.println(idx2);
+        int first = al.getFirst();
+        System.out.println("This is first Index Data " + first);
+        int last = al.getLast();
+        System.out.println("This is Last Index Data " + last);
+
+        // remove:-
+        System.out.println("Original Before remove" + al);
+        System.out.println("Remove");
+        al.remove(6);
+        System.out.println(al);
+        al.remove((Integer) 6);
         System.out.println(al);
         al.removeFirst();
+        al.removeLast();
+        System.out.println(al);
+        al.removeAll(al1);
         System.out.println(al);
     }
 }
