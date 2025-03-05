7f4afd84acc8e8dd99affe96fbe18eff0d0a3751
shubham singh <singhshubham68738@gmail.com>
2025-03-06 00:43:38 +0530

    working on list iterator

--- Cursors/ListIterator/Index.java
+++ Cursors/ListIterator/Index.java
@@ -1,12 +1,34 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Index {
     public static void main(String[] args) {
-        LinkedList<String> ll=new LinkedList<>();
+        LinkedList<String> ll = new LinkedList<>();
         ll.add("Shubham");
         ll.add("Joy");
         ll.add("Riya");
         ll.add("Harry");
         System.out.println(ll);
-    }    
+        ListIterator<String> Li = ll.listIterator();
+        while (Li.hasNext()) {
+            String s = Li.next();
+            if (s.equals("Shubham")) {
+                Li.remove();
+            }
+            if (s.equals("Joy")) {
+                Li.add("Rahul");
+            }
+            if (s.equals("Riya")) {
+                Li.add("Virat");
+            }
+            if (s.equals("Riya") && Li.hasPrevious()) {
+                String prev = Li.previous(); // Access the previous element
+                System.out.println("Previous element to 'Riya': " + prev);
+                // Move the iterator back to the current position
+                Li.next();
+                System.out.println(Li.hasNext());
+            }
+        }
+        System.out.println(ll);
+    }
 }
